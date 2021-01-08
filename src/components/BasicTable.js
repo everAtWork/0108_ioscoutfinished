c15b005088761581e8ce2c5a7152473324a9d831
Wladyslaw Ko <ever.at.work@gmail.com>
2021-01-08 15:45:58 +0200

    before I forget pt.2

--- src/components/BasicTable.js
+++ src/components/BasicTable.js
@@ -8,13 +8,46 @@ import { COLUMNS } from './columns'
 export const BasicTable = () => {
     const columns = useMemo(() => COLUMNS, [])
     const data = useMemo(() => MOCK_DATA, [])
-    useTable( {
-        columns: COLUMNS,
-        data: MOCK_DATA
+
+
+    const tableInstance = useTable({
+        columns,
+        data
     })
+    const {
+        getTableProps,getTableBodyProps,
+        headerGroups, rows, prepareRow,
+    } = tableInstance
     return (
         <div>
-            
+            <table {... getTableProps()}>
+                <thead> {headerGroups.map(headerGroup => {
+                     
+                    <tr { ... headerGroup.getHeaderGroupProps() }>
+                        {
+                            headerGroup.headers.map( column => {
+                            <th {column.getHeaderProps()}>{column.render('Header')}</th>
+
+                            })
+                }
+                </tr>
+                })}
+                </thead>
+                <tbody {... getTableBodyProps()}>
+                    {
+                        rows.map(row => {
+                            prepareRow(row)
+                            return (
+                            <tr {... row.getRowProps()}> {
+                       row.cells.map(row => {
+                           <td {... cell.getCellProps()}>{cell.render('Cell')} </td>
+                       })
+                       }
+                        </tr>)
+                        })
+                    }
+                </tbody>
+            </table>
         </div>
     )
 }
